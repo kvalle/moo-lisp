5380434b243eb5e90c3002aa3cd1c047d89bc9b2
Kjetil Valle <kjetil.valle@gmail.com>
2013-11-11 21:46:54 +0100

    refactored apply

--- moolisp/evaluator.py
+++ moolisp/evaluator.py
@@ -12,24 +12,24 @@ def evaluate(ast, env):
     if is_symbol(ast): return env[ast]
     elif is_atom(ast): return ast
     elif is_list(ast):
-        if ast[0] == 'macro': return _macro(ast, env)
-        elif ast[0] == 'expand': return _expand(ast, env)
-        elif ast[0] == 'expand-1': return _expand_1(ast, env)
-        elif ast[0] == 'cond': return _cond(ast, env)
-        elif ast[0] == 'let': return _let(ast, env)
-        elif ast[0] == 'eval': return _eval(ast, env)
-        elif ast[0] == 'set!': return _set(ast, env)
-        elif ast[0] in ('quote', 'unquote', 'quasiquote'): return _quote(ast, env)
-        elif ast[0] in ('lambda', 'λ'): return _lambda(ast, env)
-        elif ast[0] == 'atom': return _atom(ast, env)
-        elif ast[0] == 'begin': return _begin(ast, env)
-        elif ast[0] == 'define': return _define(ast, env)
-        elif ast[0] == 'if': return _if(ast, env)
-        else: return _apply(ast, env)
+        if ast[0] == 'macro': return eval_macro(ast, env)
+        elif ast[0] == 'expand': return eval_expand(ast, env)
+        elif ast[0] == 'expand-1': return eval_expand_1(ast, env)
+        elif ast[0] == 'cond': return eval_cond(ast, env)
+        elif ast[0] == 'let': return eval_let(ast, env)
+        elif ast[0] == 'eval': return eval_eval(ast, env)
+        elif ast[0] == 'set!': return eval_set(ast, env)
+        elif ast[0] in ('quote', 'unquote', 'quasiquote'): return eval_quote(ast, env)
+        elif ast[0] in ('lambda', 'λ'): return eval_lambda(ast, env)
+        elif ast[0] == 'atom': return eval_atom(ast, env)
+        elif ast[0] == 'begin': return eval_begin(ast, env)
+        elif ast[0] == 'define': return eval_define(ast, env)
+        elif ast[0] == 'if': return eval_if(ast, env)
+        else: return apply(ast, env)
     else:
         raise LispSyntaxError(ast)
 
-def _macro(ast, env):
+def eval_macro(ast, env):
     (_, params, body) = ast
     return Macro(params, body)
 
@@ -52,57 +52,57 @@ def expand_once(form, env):
     expansion = evaluate(macro.body, substitutions)
     return expansion
 
-def _expand_1(ast, env):
+def eval_expand_1(ast, env):
     form = evaluate(ast[1], env)
     if _is_macro_call(form, env):
         form = expand_once(form, env)
     return form
 
-def _expand(ast, env):
+def eval_expand(ast, env):
     form = evaluate(ast[1], env)
     while _is_macro_call(form, env):
         form = expand_once(form, env)
     return form
 
-def _cond(ast, env):
+def eval_cond(ast, env):
     for predicate, ast in ast[1:]:
         p = evaluate(predicate, env)
         _assert_boolean(p, predicate)
         if value_of(p) is True:
             return evaluate(ast, env)
 
-def _if(ast, env):
+def eval_if(ast, env):
     _assert_exp_length(ast, 4)
     (_, pred, then_exp, else_exp) = ast
     p = evaluate(pred, env)
     _assert_boolean(p, pred)
     return evaluate((then_exp if value_of(p) else else_exp), env)
 
-def _atom(ast, env):
+def eval_atom(ast, env):
     arg = evaluate(ast[1], env)
     return boolean(is_atom(arg))
 
-def _eval(ast, env):
+def eval_eval(ast, env):
     _assert_exp_length(ast, 2)
     (_, exp) = ast
     return evaluate(evaluate(exp, env), env)
 
-def _define(ast, env):
+def eval_define(ast, env):
     _assert_valid_definition(ast[1:])
     env[ast[1]] = evaluate(ast[2], env)
 
-def _lambda(ast, env):
+def eval_lambda(ast, env):
     _assert_exp_length(ast, 3)
     (_, params, body) = ast
     return Lambda(params, body, env)
 
-def _begin(ast, env):
+def eval_begin(ast, env):
     if len(ast[1:]) == 0:
         raise LispSyntaxError("begin cannot be empty: %s" % unparse(ast))
     results = [evaluate(exp, env) for exp in ast[1:]]
     return results[-1]
 
-def _quote(ast, env):
+def eval_quote(ast, env):
     def quasiquote(ast, env):
         if not isinstance(ast, list):
             return ast
@@ -120,23 +120,20 @@ def _quote(ast, env):
     elif ast[0] == 'unquote':
         raise LispSyntaxError("Unquote outside of quasiquote: %s" % unparse(ast))
 
-def _set(ast, env):
+def eval_set(ast, env):
     _assert_exp_length(ast, 3)
     (_, var, exp) = ast
     env.defining_env(var)[var] = evaluate(exp, env)
 
-def _let(ast, env):
+def eval_let(ast, env):
     _assert_exp_length(ast, 3)
     for d in ast[1]:
         _assert_valid_definition(d)
     defs = [(d[0], evaluate(d[1], env)) for d in ast[1]]
     return evaluate(ast[2], Environment(defs, env))
 
-def _apply(ast, env):
+def apply(ast, env):
     cls = evaluate(ast[0], env)
-
-    if not is_closure(cls):
-        raise LispTypeError("Call to non-function: " + unparse(ast))
 
     args = [evaluate(exp, env) for exp in ast[1:]]
     if is_lambda(cls):
@@ -148,7 +145,7 @@ def _apply(ast, env):
     elif is_builtin(cls):
         return cls.fn(*args)
     else:
-        raise Exception("Unknown implementation of Closure: %s" % cls)
+        raise LispTypeError("Call to non-function: " + unparse(ast))
 
 ## Syntax assertions
 
